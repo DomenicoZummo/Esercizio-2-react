269b2adf4b83da5e136aaec2eadda5b4f32e7e6a
DomenicoZummo <domenico.zummo@gmail.com>
2021-08-20 18:10:08 +0200

    Add dinamic class

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
 
-  const goalInputChangeHandler = event => {
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
+  const formSubmitHandler = (event) => {
+    if (enteredValue.trim().length !== 0) {
+      event.preventDefault();
+      props.onAddGoal(enteredValue);
+      setEnteredValue("");
+    } else {
+      setIsValid(false);
+      event.preventDefault();
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label className={!isValid ? "not-valid" : "valid"}>Course Goal</label>
+        <input
+          type="text"
+          value={enteredValue}
+          className={!isValid ? "not-valid" : "valid"}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
